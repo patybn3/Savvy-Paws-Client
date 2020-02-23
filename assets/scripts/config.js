0a5174d851134ba3c4367a2a1680cf80740e9bba
patybn3 <paty.nigro@gmail.com>
2020-02-23 16:05:42 -0500

    Add Buttons to HTML, logo, main columns
    New file, variables.scss for colors,
    config file adjusted with Heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://tranquil-earth-29477.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
